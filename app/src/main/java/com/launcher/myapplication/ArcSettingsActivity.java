6bad541e400e0b74fc585f7a201c9530188aad1a
XEMBO <nishadvishal937@gamil.com>
2023-05-20 18:39:26 +0530

    darkmode enable disable working now

--- app/src/main/java/com/launcher/myapplication/ArcSettingsActivity.java
+++ app/src/main/java/com/launcher/myapplication/ArcSettingsActivity.java
@@ -1,5 +1,7 @@
 package com.launcher.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -12,41 +14,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
-
 public class ArcSettingsActivity extends AppCompatActivity {
 
     SwitchMaterial DarkmodeSwitch;
     SwitchMaterial Switch_Clock;
+    boolean darkMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sEditor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arc_settings);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
         DarkmodeSwitch = findViewById(R.id.darkmode);
         Switch_Clock = findViewById(R.id.Arc_clock_switch);
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        darkMode = sharedPreferences.getBoolean("dark", true);
+
+        if (darkMode) {
+            DarkmodeSwitch.setChecked(true);
+        }
 
         DarkmodeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
+                if (b) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
-                else
+                    sEditor = sharedPreferences.edit();
+                    sEditor.putBoolean("dark", true);
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            }
-        });
-        Switch_Clock.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                  showClock();
+                    sEditor = sharedPreferences.edit();
+                    sEditor.putBoolean("dark", false);
                 }
-                else
-                    hideClock();
+                sEditor.apply(); // Apply the changes to SharedPreferences
             }
         });
 
+        Switch_Clock.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    showClock();
+                } else {
+                    hideClock();
+                }
+            }
+        });
     }
+
+    // ... your existing code ...
+
+
+
     private void hideClock() {
         // Hide the clock view
         LinearLayout clockView = findViewById(R.id.clock_date);
@@ -58,7 +85,8 @@ public class ArcSettingsActivity extends AppCompatActivity {
     private void showClock() {
         // Show the clock view
         LinearLayout clockView = findViewById(R.id.clock_date);
-        if (clockView != null)
+        if (clockView != null) {
             clockView.setVisibility(View.VISIBLE);
         }
     }
+}
