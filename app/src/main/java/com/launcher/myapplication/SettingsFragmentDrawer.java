144be8cb62389de678eb5b69ba74f9053e2076fd
XEMBO <nishadvishal937@gamil.com>
2023-05-22 16:02:56 +0530

    succes touch handle on seekbar

--- app/src/main/java/com/launcher/myapplication/SettingsFragmentDrawer.java
+++ app/src/main/java/com/launcher/myapplication/SettingsFragmentDrawer.java
@@ -2,12 +2,17 @@ package com.launcher.myapplication;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.marcinmoskala.arcseekbar.ArcSeekBar;
 
 public class SettingsFragmentDrawer extends AppCompatActivity {
 
@@ -30,7 +35,30 @@ public class SettingsFragmentDrawer extends AppCompatActivity {
     public static class SettingsFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.settings_drawer, container, false);
+            View rootView = inflater.inflate(R.layout.settings_drawer, container, false);
+
+            ArcSeekBar seekBar = rootView.findViewById(R.id.seekArc);
+            LinearLayout rootViewLayout = rootView.findViewById(R.id.rootlayout);
+
+            seekBar.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    int action = motionEvent.getAction();
+                    if (action == MotionEvent.ACTION_DOWN) {
+                        // Disable touch events for other views
+                        rootViewLayout.requestDisallowInterceptTouchEvent(true);
+                    } else if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL) {
+                        // Enable touch events for other views
+                        rootViewLayout.requestDisallowInterceptTouchEvent(false);
+                    }
+                    // Return false to allow the ArcSeekBar to handle its own touch events
+                    return false;
+                }
+            });
+
+            return rootView;
         }
     }
+
+
 }
