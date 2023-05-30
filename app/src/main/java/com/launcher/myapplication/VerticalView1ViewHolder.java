5de9d1b8bbe370f74f708c9f58e27d4b95cececc
XEMBO <nishadvishal937@gamil.com>
2023-05-30 15:58:24 +0530

    stash

--- app/src/main/java/com/launcher/myapplication/VerticalView1ViewHolder.java
+++ app/src/main/java/com/launcher/myapplication/VerticalView1ViewHolder.java
@@ -1,5 +1,6 @@
 package com.launcher.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,14 +11,24 @@ import android.content.pm.ResolveInfo;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.marcinmoskala.arcseekbar.ArcSeekBar;
 
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 
@@ -129,6 +140,178 @@ public class VerticalView1ViewHolder extends RecyclerView.ViewHolder {
             vibrator.vibrate(50);
         }
     }
+
+
+
+
+
+
+
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+
+
+            View view = findViewById(R.id.homescreen);
+            vibrate();
+
+
+            float x =e.getRawX();
+            float y = e.getRawY();
+
+
+            // Create a new popup window
+            PopupWindow popupWindow = new PopupWindow(ActivityHome.this);
+            popupWindow.setBackgroundDrawable(null);
+            // Set the content view of the popup window
+            View popupView = LayoutInflater.from(ActivityHome.this).inflate(R.layout.popup_layout, null);
+            popupWindow.setContentView(popupView);
+
+            // Find the view inside the popup layout and set an onClickListener to it
+            ImageButton wallpaper = popupView.findViewById(R.id.wallpaper);
+
+            ImageButton arcSettingsButton = popupView.findViewById(R.id.ArcSettings);
+            ImageButton widgetsButton = popupView.findViewById(R.id.Widgets);
+            wallpaper.setOnClickListener(v -> {
+
+                //popup setwallpaper
+
+                Intent intent = new Intent(Intent.ACTION_SET_WALLPAPER);
+                context.startActivity(Intent.createChooser(intent, "Select Wallpaper"));
+                popupWindow.dismiss();
+
+
+            });
+
+            arcSettingsButton.setOnClickListener(v -> {
+
+                //popup ArcSettingsbutton
+
+                Intent intent = new Intent(ActivityHome.this, ArcSettingsActivity.class);
+                context.startActivity(intent);
+                popupWindow.dismiss();
+            });
+
+
+            widgetsButton.setOnClickListener(v -> {
+
+
+                //popup Widgets
+
+                Intent intent = new Intent(Intent.ACTION_CREATE_SHORTCUT);
+                context.startActivity(Intent.createChooser(intent, "Select Widget"));
+                popupWindow.dismiss();
+            });
+            // Set the size of the popup window
+            popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+            popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            // Make the popup window dismiss when the user taps outside of it or presses the back button
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setFocusable(true);
+            int verticalOffset = 200;
+
+            // Show the popup window at the location of the long press event
+            popupWindow.showAtLocation(view, Gravity.NO_GRAVITY, (int) x, (int) y-verticalOffset);
+
+
+
+            // Set a click listener on the content view of the popup window to dismiss it when the user taps anywhere inside it
+            popupView.setOnClickListener(v -> popupWindow.dismiss());
+
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    vibrate();
+                    if (diffX > 0) {
+                        Intent RightSwipe = new Intent(Intent.ACTION_DIAL);
+                        context.startActivity(RightSwipe);
+                    } else {
+                        Intent LeftSwipe = new Intent(Intent.ACTION_MAIN);
+                        LeftSwipe.addCategory(Intent.CATEGORY_APP_MESSAGING);
+                        context.startActivity(LeftSwipe);
+                    }
+                }
+            } else {
+                if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        ExpandNotificationBar();
+                    } else {
+                        vibrate();
+                        View mDrawerSheet = findViewById(R.id.DrawerSheet);
+                        final BottomSheetBehavior<View> mDrawerSheetBehaviour = BottomSheetBehavior.from(mDrawerSheet);
+                        mDrawerSheetBehaviour.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    }
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent event) {
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent event) {
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent event) {
+
+            Intent Viewpager = new Intent(ActivityHome.this, ActivityPager.class);
+            context.startActivity(Viewpager);
+
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent event) {
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent event) {
+            return true;
+        }
+
+        private void ExpandNotificationBar() {
+            try {
+                @SuppressLint("WrongConstant") Object service = context.getSystemService("statusbar");
+                Class<?> statusBarManager = Class.forName("android.app.StatusBarManager");
+                Method expand = statusBarManager.getMethod("expandNotificationsPanel");
+                expand.invoke(service);
+            } catch (Exception e) {
+                String errorMessage = "Notification panel swipe down error: " + e.getMessage();
+                Log.e("StatusBar", errorMessage);
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
 
     // Rest of the class implementation...
 }
